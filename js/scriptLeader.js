4d87e5ab1365a7702caad0168f2a36088f10d508
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-03-15 21:50:51 +0700

    add search feature

--- js/scriptLeader.js
+++ js/scriptLeader.js
@@ -1,7 +1,10 @@
 new Vue({
   el:"#leaderboard",
   data: {
-    data_leader: []
+    data_leader: [],
+    ordering: 'descending',
+    search: '',
+    data_search: []
   },
   mounted:function(){
     this.addLeader()
@@ -13,6 +16,29 @@ new Vue({
         response.data.data.reverse().forEach((data,i) => {
           app.data_leader.push(data)
         })
+      })
+    },
+    changeOrder: function () {
+      if (this.ordering === 'descending') {
+        this.data_leader.reverse()
+        this.ordering = 'ascending'
+      }
+      else {
+        this.data_leader.reverse()
+        this.ordering = 'descending'
+      }
+    }
+  },
+  watch: {
+    search: function (val) {
+      console.log(val);
+      let app = this
+      app.data_search = []
+      this.data_leader.forEach(data => {
+        let searchName = data.name.toLowerCase().search(val)
+        if (searchName >= 0) {
+          app.data_search.push(data)
+        }
       })
     }
   }
