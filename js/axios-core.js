1d7a5f46f4e01d1fec2be257ab9fe70a92d73951
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-16 09:59:34 +0700

    Give conditon to axios base url

--- js/axios-core.js
+++ js/axios-core.js
@@ -1,3 +1,11 @@
-var request = axios.create({
-  baseURL: 'http://localhost:3000',
-});
+const hostname = document.location.hostname;
+console.log(hostname)
+if (hostname == '127.0.0.1' || hostname == 'localhost')  {
+  const request = axios.create({
+    baseURL: 'http://localhost:3000',
+  });
+} else {
+  const request = axios.create({
+    baseURL: 'http://stf-api.geekosta.com',
+  });
+}
